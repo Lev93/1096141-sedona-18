36f6cd90f54f04338af930554233a36a5db72646
Lev Riabov <levriabov12345@mail.ru>
2019-12-15 18:36:28 +0300

    финал

---
+++ source/js/mainmenu.js
@@ -0,0 +1,49 @@
+var mainMenu = document.querySelector(".main-menu");
+var button = document.querySelector(".main-menu__toggle");
+
+button.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (mainMenu.classList.contains('main-menu--opened')) {
+  mainMenu.classList.remove("main-menu--opened");
+  button.classList.add("main-menu__toggle--closed");
+  }else {
+    mainMenu.classList.add("main-menu--opened");
+    button.classList.remove("main-menu__toggle--closed");
+  };
+});
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  var mapEl = document.querySelector('#map');
+
+  if (mapEl) {
+    ymaps.ready(init); // активируем карту после загрузки страницы
+    var map;
+    function init() {
+        map = new ymaps.Map("map", { // в кавычках id элемента куда загружается карта
+            center: [34.8696661, -111.7611616],
+            zoom: 7,
+            controls: []
+        });
+        var placemark = new ymaps.Placemark([34.8696661, -111.7611616], {}, { // координаты метки
+            iconLayout: "default#image",
+            iconImageHref: "img/map-marker.svg", // иконка метки
+            iconImageSize: [27, 27] // размер метки
+        });
+        map.geoObjects.add(placemark);
+    }
+  }
+});
+
+var sendForm = document.querySelector("button--send-form");
+var popupSuccess = document.querySelector(".popup--success");
+var closePopup = document.querySelector(".button--popup-close");
+
+sendForm.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popupSuccess.classList.remove("popup--closed");
+});
+
+closePopup.addEventListener("click", function (evt2) {
+  evt2.preventDefault();
+  popupSuccess.classList.add("popup--closed");
+});

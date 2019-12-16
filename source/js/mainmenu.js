adf6ba4b02934d7f76050fa7a6ee4b0890cf4c24
Lev Riabov <levriabov12345@mail.ru>
2019-12-16 22:28:05 +0300

    1

--- source/js/mainmenu.js
+++ source/js/mainmenu.js
@@ -1,5 +1,5 @@
-let mainMenu = document.querySelector(".main-menu");
-let button = document.querySelector(".main-menu__toggle");
+var mainMenu = document.querySelector(".main-menu");
+var button = document.querySelector(".main-menu__toggle");
 
 button.addEventListener("click", function (e) {
   e.preventDefault();
@@ -12,129 +12,38 @@ button.addEventListener("click", function (e) {
   };
 });
 
-(function(){
-  var form = document.querySelector('.review-form'),
-      inputField = form.querySelectorAll('.text-field__input'),
-      sendForm = form.querySelector('.button--send-form'),
-      popupError = form.querySelector('.popup--error'),
-      popupSuccess = form.querySelector('.popup--success'),
-      closePopup = form.querySelectorAll('.button--popup-close');
+document.addEventListener("DOMContentLoaded", function(event) {
+  var mapEl = document.querySelector("#map");
 
-
-  for (var l = 0; l < inputField.length; l++) {
-
-    inputField[l].addEventListener('focus', function() {
-      this.classList.remove('error');
-      var parent = this.parentNode;
-      var message = parent.querySelector('.error-text');
-      if(message) parent.removeChild(message);
-
-      var helpText = this.getAttribute('title');
-      if(!helpText) return;
-
-      var helpBlock = document.createElement('div');
-
-      helpBlock.textContent = helpText;
-      helpBlock.className = 'help-message';
-      this.parentNode.appendChild(helpBlock);
-    });
-
-    inputField[l].addEventListener('blur', function() {
-      var helpBlock = form.querySelector('.help-message');
-      if(helpBlock) this.parentNode.removeChild(helpBlock);
-    });
-  }
-
-  function showCover() {
-    var coverDiv = document.createElement('div');
-    coverDiv.classList.add('cover-div');
-    document.body.appendChild(coverDiv);
-  };
-
-  function hideCover() {
-    document.body.removeChild(document.querySelector('.cover-div'));
-  };
-
-
-  sendForm.addEventListener('click', validate);
-
-  function validate(evt) {
-    evt.preventDefault();
-
-    var inputs = form.querySelectorAll('[data-error]');
-
-    clear(inputs, function() {
-      checkIsEmpty(inputs);
-    });
-
-    function checkIsEmpty(inputs) {
-      var isEmpty = false;
-
-      for (var i = 0; i < inputs.length; i++) {
-        var input = inputs[i];
-
-        var pattern = input.getAttribute('pattern');
-
-        if (!pattern) pattern = '^[А-Яа-яЁё \s \n \. \, \;]+$' ;
-
-        pattern = new RegExp(pattern);
-
-        if(pattern.test(input.value) !== true) {
-            isEmpty = true;
-            markInput(input);
-            showCover();
-            popupError.classList.add('popup--show');
-            return false;
-        }
-      }
-      showCover();
-      popupSuccess.classList.add('popup--show');
-    }
-
-    function markInput(input) {
-      input.classList.add('error');
-      var text = input.getAttribute('data-error');
-
-      if (!text) return;
-
-      var div = document.createElement('div');
-
-      div.textContent = text;
-      div.className = 'error-text';
-      input.parentNode.appendChild(div);
-    }
-
-    function clear(inputsItem, callback) {
-      for (var i = 0; i < inputsItem.length; i++) {
-        var input = inputsItem[i];
-        var parent = input.parentNode;
-        var message = parent.querySelector('.error-text');
-
-        input.classList.remove('error');
-        if(message) parent.removeChild(message);
-      }
-
-      if(callback) callback();
+  if (mapEl) {
+    ymaps.ready(init); // активируем карту после загрузки страницы
+    var map;
+    function init() {
+        map = new ymaps.Map("map", { // в кавычках id элемента куда загружается карта
+            center: [34.8696661, -111.7611616],
+            zoom: 7,
+            controls: []
+        });
+        var placemark = new ymaps.Placemark([34.8696661, -111.7611616], {}, { // координаты метки
+            iconLayout: "default#image",
+            iconImageHref: "img/map-marker.svg", // иконка метки
+            iconImageSize: [27, 27] // размер метки
+        });
+        map.geoObjects.add(placemark);
     }
   }
+});
 
-  window.addEventListener('keydown', function(evt) {
-    if (evt.keyCode === 27 || evt.keyCode === 13) {
-      if (popupError.classList.contains('popup--show') || popupSuccess.classList.contains('popup--show')) {
-        evt.preventDefault();
-        hideCover();
-        popupError.classList.remove('popup--show');
-        popupSuccess.classList.remove('popup--show');
-      }
-    }
-  });
+var sendForm = document.querySelector(".button--send-form");
+var popupSuccess = document.querySelector(".popup--success");
+var closePopup = document.querySelector(".button--popup-close");
 
-  for (var i = 0; i < closePopup.length; i++) {
-    closePopup[i].addEventListener('click', function(evt) {
-      evt.preventDefault();
-      hideCover();
-      popupError.classList.remove('popup--show');
-      popupSuccess.classList.remove('popup--show');
-    });
-  };
-}());
+sendForm.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popupSuccess.classList.remove("popup--closed");
+});
+
+closePopup.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popupSuccess.classList.add("popup--closed");
+});
